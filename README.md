432076d0fb45fef5ea68d2fdea8b4b0f9bad7fac
Kevin Buhrer <kevin@yellowtugboat.com>
2016-10-10 10:57:04 -0600

    Update documentation and README generation.

--- README.md
+++ README.md
@@ -1,6 +1,88 @@
 # eslint-config-yellow-tugboat
 Linting rules based on the Yellow Tugboat coding standards.
 
+You can install this module using npm:
+`npm install eslint-config-yellow-tugboat`
+
 ## About
 The linting rules here are primarily further restrictions of `eslint:recommended` with the intent
 of creating even more consistent and readable code.
+
+<a name="RulesDef"></a>
+
+## RulesDef : <code>object</code>
+**Kind**: global namespace  
+
+* [RulesDef](#RulesDef) : <code>object</code>
+    * [.config](#RulesDef.config)
+    * [.EslintRule](#RulesDef.EslintRule) : <code>string</code> &#124; <code>array</code>
+
+<a name="RulesDef.config"></a>
+
+### RulesDef.config
+The rule configuration.
+
+**Kind**: static constant of <code>[RulesDef](#RulesDef)</code>  
+**Properties**
+
+| Name | Type | Description |
+| --- | --- | --- |
+| extends | <code>string</code> | Ruleset extends _eslint:recommended_ |
+| rules | <code>object</code> | The rules that customize beyond the base set. |
+| rules.indent | <code>EslintRule</code> | Require two spaces for consistency. |
+| rules.linebreak-style | <code>EslintRule</code> | Use consistent line endings. |
+| rules.camelcase | <code>EslintRule</code> | Use consistent variable casing. |
+| rules.quotes | <code>EslintRule</code> | Encourage single quotes for consistency. Double quotes are more likely to be quoted and have more visual weight. |
+| rules.semi | <code>EslintRule</code> | Semicolons add clarity. |
+| rules.comma-dangle | <code>EslintRule</code> | Comma dangles simplify diffs. |
+| rules.curly | <code>EslintRule</code> | Curly braces clarify intent. |
+| rules.no-caller | <code>EslintRule</code> | Rarely applicable or allowed. |
+| rules.brace-style | <code>EslintRule</code> | Use Stroustrup style to create distinct block-sections, but remain somewhat condensed. |
+| rules.no-whitespace-before-property | <code>EslintRule</code> | Discourage superfluous whitespace. Inline whitespace should occur only as necessary. |
+| rules.no-multi-spaces | <code>EslintRule</code> | Inline whitespace should be simple. |
+| rules.no-multiple-empty-lines | <code>EslintRule</code> | Multiple lines create excessive separation removing block context from operation. Many lines for clarity implies refactoring should occur. |
+| rules.key-spacing | <code>EslintRule</code> | Have uniform object declarations. |
+| rules.no-trailing-spaces | <code>EslintRule</code> | Discourage superfluous whitespace. |
+| rules.func-call-spacing | <code>EslintRule</code> | Discourage superfluous whitespace. |
+| rules.padded-blocks | <code>EslintRule</code> | Discourage whitespace that separates block context from operations within the block. |
+| rules.space-before-blocks | <code>EslintRule</code> | Require separation between keywords and blocks for readability. |
+| rules.keyword-spacing | <code>EslintRule</code> | Be consistent and always require it for readability. |
+| rules.space-unary-ops | <code>EslintRule</code> | Word operators always need a space to be readable, nonword operators should always be attached to what they are operating on for clarity. |
+| rules.spaced-comment | <code>EslintRule</code> | Have standardized comments |
+| rules.no-unsafe-negation | <code>EslintRule</code> | Encourage clarity around negation. |
+| rules.no-global-assign | <code>EslintRule</code> | Do not allow replacing of built-in values / types. |
+| rules.no-var | <code>EslintRule</code> | Use let of const preferred |
+| rules.no-floating-decimal | <code>EslintRule</code> | Ensure number clarity |
+| rules.no-implicit-globals | <code>EslintRule</code> | Use reasonable scoping |
+| rules.no-eval | <code>EslintRule</code> | Avoid dangerous coding |
+| rules.no-implied-eval | <code>EslintRule</code> | Avoid dangerous coding |
+| rules.no-label-var | <code>EslintRule</code> | Avoid confusion |
+| rules.no-labels | <code>EslintRule</code> | Generally should not be used for loops (refactor instead), never allowed for Switch statements |
+| rules.max-len | <code>EslintRule</code> | Prevent code from being excessively wide, but allow specific exceptions. |
+| rules.max-depth | <code>EslintRule</code> | Prevent overly nested blocks. |
+| rules.max-statements | <code>EslintRule</code> | Prevent excessive function length. |
+| rules.complexity | <code>EslintRule</code> | Limit cyclomatic complexity |
+| rules.no-confusing-arrow | <code>EslintRule</code> | Clarity is important. |
+| rules.arrow-parens | <code>EslintRule</code> | Require parens always for consistency. |
+| rules.arrow-body-style | <code>EslintRule</code> | Simple arrow expressions are easy to read and can be convenient to read properties. |
+| rules.arrow-spacing | <code>EslintRule</code> | Require reasonable whitespace consistent with other spacing rules. |
+| rules.no-fallthrough | <code>EslintRule</code> | Require explicit fall through on `Switches`. |
+| rules.no-unneeded-ternary | <code>EslintRule</code> | Keep it simple whenever possible. |
+| rules.no-lonely-if | <code>EslintRule</code> | Limit unnecessary nesting. |
+| rules.no-else-return | <code>EslintRule</code> | Do not use if-else for multiple returns. Use implicit else block to limit visual artifacts or if the dual-block adds clarity then assign output to a temporary variable. |
+| rules.no-warning-comments | <code>EslintRule</code> | Don't forget there are 'TODO's and 'FIXME's, but don't error on them just nag. |
+
+<a name="RulesDef.EslintRule"></a>
+
+### RulesDef.EslintRule : <code>string</code> &#124; <code>array</code>
+An ESLint rule where a "level" [string](string) is accepted in the simpliest case and [array](array) with
+ordered items in the more complex. The items are "level" [string](string), "top-priority value" [any](any),
+and "other parameters" [object](object). See ESLint documentation for specific rules.
+
+**Kind**: static typedef of <code>[RulesDef](#RulesDef)</code>  
+
+## License
+MIT
+
+## Copyright
+&copy; 2016 Yellow Tugboat
--- index.js
+++ index.js
@@ -1,28 +1,88 @@
-module.exports = {
+/**
+ * @namespace RulesDef
+ */
+
+/**
+ * An ESLint rule where a "level" {@link string} is accepted in the simpliest case and {@link array} with
+ * ordered items in the more complex. The items are "level" {@link string}, "top-priority value" {@link any},
+ * and "other parameters" {@link object}. See ESLint documentation for specific rules.
+ * @typedef {(string|array)} EslintRule
+ * @memberof RulesDef
+ */
+
+/**
+ * The rule configuration.
+ * @memberof RulesDef
+ * @prop {string} extends - Ruleset extends _eslint:recommended_
+ * @prop {object} rules - The rules that customize beyond the base set.
+ * @prop {EslintRule} rules.indent - Require two spaces for consistency.
+ * @prop {EslintRule} rules.linebreak-style - Use consistent line endings.
+ * @prop {EslintRule} rules.camelcase - Use consistent variable casing.
+ * @prop {EslintRule} rules.quotes - Encourage single quotes for consistency. Double quotes are more likely to be quoted and have more visual weight.
+ * @prop {EslintRule} rules.semi - Semicolons add clarity.
+ * @prop {EslintRule} rules.comma-dangle - Comma dangles simplify diffs.
+ * @prop {EslintRule} rules.curly - Curly braces clarify intent.
+ * @prop {EslintRule} rules.no-caller - Rarely applicable or allowed.
+ * @prop {EslintRule} rules.brace-style - Use Stroustrup style to create distinct block-sections, but remain somewhat condensed.
+ * @prop {EslintRule} rules.no-whitespace-before-property - Discourage superfluous whitespace. Inline whitespace should occur only as necessary.
+ * @prop {EslintRule} rules.no-multi-spaces - Inline whitespace should be simple.
+ * @prop {EslintRule} rules.no-multiple-empty-lines - Multiple lines create excessive separation removing block context from operation. Many lines for clarity implies refactoring should occur.
+ * @prop {EslintRule} rules.key-spacing - Have uniform object declarations.
+ * @prop {EslintRule} rules.no-trailing-spaces - Discourage superfluous whitespace.
+ * @prop {EslintRule} rules.func-call-spacing - Discourage superfluous whitespace.
+ * @prop {EslintRule} rules.padded-blocks - Discourage whitespace that separates block context from operations within the block.
+ * @prop {EslintRule} rules.space-before-blocks - Require separation between keywords and blocks for readability.
+ * @prop {EslintRule} rules.keyword-spacing - Be consistent and always require it for readability.
+ * @prop {EslintRule} rules.space-unary-ops - Word operators always need a space to be readable, nonword operators should always be attached to what they are operating on for clarity.
+ * @prop {EslintRule} rules.spaced-comment - Have standardized comments
+ * @prop {EslintRule} rules.no-unsafe-negation - Encourage clarity around negation.
+ * @prop {EslintRule} rules.no-global-assign - Do not allow replacing of built-in values / types.
+ * @prop {EslintRule} rules.no-var - Use let of const preferred
+ * @prop {EslintRule} rules.no-floating-decimal - Ensure number clarity
+ * @prop {EslintRule} rules.no-implicit-globals - Use reasonable scoping
+ * @prop {EslintRule} rules.no-eval - Avoid dangerous coding
+ * @prop {EslintRule} rules.no-implied-eval - Avoid dangerous coding
+ * @prop {EslintRule} rules.no-label-var - Avoid confusion
+ * @prop {EslintRule} rules.no-labels - Generally should not be used for loops (refactor instead), never allowed for Switch statements
+ * @prop {EslintRule} rules.max-len - Prevent code from being excessively wide, but allow specific exceptions.
+ * @prop {EslintRule} rules.max-depth - Prevent overly nested blocks.
+ * @prop {EslintRule} rules.max-statements - Prevent excessive function length.
+ * @prop {EslintRule} rules.complexity - Limit cyclomatic complexity
+ * @prop {EslintRule} rules.no-confusing-arrow - Clarity is important.
+ * @prop {EslintRule} rules.arrow-parens - Require parens always for consistency.
+ * @prop {EslintRule} rules.arrow-body-style - Simple arrow expressions are easy to read and can be convenient to read properties.
+ * @prop {EslintRule} rules.arrow-spacing - Require reasonable whitespace consistent with other spacing rules.
+ * @prop {EslintRule} rules.no-fallthrough - Require explicit fall through on `Switches`.
+ * @prop {EslintRule} rules.no-unneeded-ternary - Keep it simple whenever possible.
+ * @prop {EslintRule} rules.no-lonely-if - Limit unnecessary nesting.
+ * @prop {EslintRule} rules.no-else-return - Do not use if-else for multiple returns. Use implicit else block to limit visual artifacts or if the dual-block adds clarity then assign output to a temporary variable.
+ * @prop {EslintRule} rules.no-warning-comments - Don't forget there are 'TODO's and 'FIXME's, but don't error on them just nag.
+ */
+const config = {
   'extends': 'eslint:recommended',
   'rules': {
     // General Formatting
-    'indent': [ 'error', 2 ], // Require two spaces for consistency
-    'linebreak-style': [ 'error', 'unix' ], // Use consistent line endings.
-    'camelcase': 'error', // Use consistent variable casing.
-    'quotes': [ 'error', 'single' ], // Encourage single quotes for consistency. Double quotes are more likely to be quoted and have more visual weight.
-    'semi': [ 'error', 'always' ], // Semicolons add clarity.
-    'comma-dangle': [ 'error', 'always-multiline' ], // Comma dangles simplify diffs.
-    'curly': 'error', // Curly braces clarify intent.
-    'no-caller': 'error', // Rarely applicable or allowed.
-    'brace-style': ['error', 'stroustrup', { 'allowSingleLine': true }], // Use Stroustrup style to create distinct block-sections, but remain somewhat condensed.
+    'indent': [ 'error', 2 ],
+    'linebreak-style': [ 'error', 'unix' ],
+    'camelcase': 'error',
+    'quotes': [ 'error', 'single' ],
+    'semi': [ 'error', 'always' ],
+    'comma-dangle': [ 'error', 'always-multiline' ],
+    'curly': 'error',
+    'no-caller': 'error',
+    'brace-style': ['error', 'stroustrup', { 'allowSingleLine': true }],
 
     // Whitespace
-    'no-whitespace-before-property': 'error', // Discourage superfluous whitespace. Inline whitespace should occur only as necessary.
-    'no-multi-spaces': 'error', // Inline whitespace should be simple.
-    'no-multiple-empty-lines': 'error', // Multiple lines create excessive separation removing block context from operation. Many lines for clarity implies refactoring should occur.
-    'key-spacing': 'error', // Have uniform object declarations.
-    'no-trailing-spaces': 'error', // Discourage superfluous whitespace.
-    'func-call-spacing': 'error', // Discourage superfluous whitespace.
-    'padded-blocks': ['error', 'never'], // Discourage whitespace that separates block context from operations within the block.
-    'space-before-blocks': 'error', // Require separation between keywords and blocks for readability.
-    'keyword-spacing': 'error', // Be consistent and always require it for readability.
-    'space-unary-ops': 'error', // Word operators always need a space to be readable, nonword operators should always be attached to what they are operating on for clarity.
+    'no-whitespace-before-property': 'error',
+    'no-multi-spaces': 'error',
+    'no-multiple-empty-lines': 'error',
+    'key-spacing': 'error',
+    'no-trailing-spaces': 'error',
+    'func-call-spacing': 'error',
+    'padded-blocks': ['error', 'never'],
+    'space-before-blocks': 'error',
+    'keyword-spacing': 'error',
+    'space-unary-ops': 'error',
     'spaced-comment': ['error', 'always', {
       'line': {
         'markers': ['/'],
@@ -33,18 +93,18 @@ module.exports = {
         'exceptions': ['*'],
         'balanced': true,
       },
-    }], // Have standardized comments
+    }],
 
     // Code features
-    'no-unsafe-negation': 'error', // Encourage clarity around negation.
-    'no-global-assign': 'error', // Do not allow replacing of built-in values / types.
-    'no-var': 'error',  // Use let of const preferred
-    'no-floating-decimal': 'error', // Ensure number clarity
-    'no-implicit-globals': 'error', // Use reasonable scoping
-    'no-eval': 'error', // Avoid dangerous coding
-    'no-implied-eval': 'error', // Avoid dangerous coding
-    'no-label-var': 'error', // Avoid confusion
-    'no-labels': ['error', { 'allowLoop': true }], // Generally should not be used for loops (refactor instead), never allowed for Switch statements
+    'no-unsafe-negation': 'error',
+    'no-global-assign': 'error',
+    'no-var': 'error',
+    'no-floating-decimal': 'error',
+    'no-implicit-globals': 'error',
+    'no-eval': 'error',
+    'no-implied-eval': 'error',
+    'no-label-var': 'error',
+    'no-labels': ['error', { 'allowLoop': true }],
     'max-len': ['error', {
       'code': 100,
       'tabWidth': 2,  // Set to match indent, but no tabs are expected to be used.
@@ -52,28 +112,30 @@ module.exports = {
       'ignoreUrls': true,
       'ignoreStrings': true,
       'ignoreTemplateLiterals': true,
-    }], // Prevent code from being excessively wide, but allow specific exceptions.
+    }],
     'max-depth': 'error',
-    'max-statements': ['error', 20], // Prevent excessive function length.
-    'complexity': ['error', 15], // Limit cyclomatic complexity
+    'max-statements': ['error', 20],
+    'complexity': ['error', 15],
 
 
     // Arrow Functions
-    'no-confusing-arrow': ['error', {'allowParens': false}], // Clarity is important.
-    'arrow-parens': 'error',  // Require parens always for consistency.
-    'arrow-body-style': ['error', 'as-needed'], // Simple arrow expressions are easy to read and can be convenient to read properties.
-    'arrow-spacing': 'error', // Require reasonable whitespace consistent with other spacing rules.
+    'no-confusing-arrow': ['error', {'allowParens': false}],
+    'arrow-parens': 'error',
+    'arrow-body-style': ['error', 'as-needed'],
+    'arrow-spacing': 'error',
 
     // Switch Statements
     // Switches are generally discouraged, but should be done as safe and explicitly as possible.
-    'no-fallthrough': 'error', // Require explicit fall through.
+    'no-fallthrough': 'error',
 
     // If Statements
-    'no-unneeded-ternary': 'error', // Keep it simple whenever possible.
-    'no-lonely-if': 'error', // Limit unnecessary nesting.
-    'no-else-return': 'error',  // Do not use if-else for multiple returns. Use implicit else block to limit visual artifacts or if the dual-block adds clarity then assign output to a temporary variable.
+    'no-unneeded-ternary': 'error',
+    'no-lonely-if': 'error',
+    'no-else-return': 'error',
 
     // Reminders
-    'no-warning-comments': 'warn', // Don't forget there are 'TODO's and 'FIXME's, but don't error on them just nag.
+    'no-warning-comments': 'warn',
   },
 };
+
+module.exports = config;
